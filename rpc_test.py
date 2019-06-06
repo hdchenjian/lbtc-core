271ce205ff427328bd0961dd5a275259386707b2
chen jian <jian.chen@iim.ltd>
2019-06-06 16:51:25 +0800

    add test

--- rpc_test.py
+++ rpc_test.py
@@ -8,20 +8,36 @@ def update_rpc_node():
     ./bitcoind -testnet -txreindex -rpcuser=luyao -rpcpassword=DONNNN
     '''
     rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:19332" % ('luyao', 'DONNNN'))
+    print(rpc_connection.createrawtransaction([{"txid": "db949bca827cbe1f5f18848cf55cbc2eedd3cf9633fb9b88e546171087cef4ff", "vout": 0}],
+                                              {"mtUjv2TWT2wMQ86LoshoK5U8dPTL213Zep": 0.0001}))
+    '''
     print(rpc_connection.signmessagewithprivkey('cQ82WpB4FLdszixBJGbPkAEBVxwgaLJsPaUQxFduhMgYcTbAUzK5', 'hiiiiiiiiiiiiiiiiiiiiiiiii'))
     print(rpc_connection.verifymessage('mftBcEPBkrKZ8Gys5yyDfq8jXCydjf5MtZ',
                           'Hzbimu4CcxPujU4f9911jza4Ptkmpu4JZHKgYlPyHshnXfN4Mi0QLiy0xKD/a2tcWsEBCQEdxRg78uNnmlBMemQ=',
                           'hi'))
+    '''
     #print(rpc_connection.getbestblockhash())
     #print(rpc_connection.getblock(rpc_connection.getbestblockhash()))
     #print(rpc_connection.addnode('47.96.169.139:19333', 'add'))
+    #print(rpc_connection.addnode('47.91.209.185:19333', 'add'))
+    #print(rpc_connection.addnode('47.91.209.185:19333', 'remove'))
     #print(rpc_connection.addnode('192.168.1.139:19333', 'add'))
-    #print(rpc_connection.addnode('192.168.1.139:19333', 'remove'))
+    #print(rpc_connection.addnode('192.168.1.102:19333', 'remove'))
+    #print(rpc_connection.addnode('192.168.1.102:19333', 'add'))
     #print(rpc_connection.getpeerinfo())
     #print(rpc_connection.getnewaddress('lbtc'))
     #print(rpc_connection.getaccountaddress('lbtc'))
     #print(rpc_connection.getbalance())
     #print(rpc_connection.getwalletinfo())
+    '''
+    print(rpc_connection.listaccounts())
+    print(rpc_connection.listaddressgroupings())
+    print(rpc_connection.listreceivedbyaccount())
+    print(rpc_connection.listreceivedbyaddress())
+    '''
+    #print(rpc_connection.listtransactions())
+    #print(rpc_connection.gettransactionnew('db949bca827cbe1f5f18848cf55cbc2eedd3cf9633fb9b88e546171087cef4ff'))
+    #print(rpc_connection.listunspent()[-1])
     #print(rpc_connection.importprivkey('cQ82WpB4FLdszixBJGbPkAEBVxwgaLJsPaUQxFduhMgYcTbAUzK5', 'lbtc'))
     #print(rpc_connection.getaccount('mftBcEPBkrKZ8Gys5yyDfq8jXCydjf5MtZ'))
     #print(rpc_connection.listdelegates())
@@ -29,10 +45,9 @@ def update_rpc_node():
     #print(rpc_connection.vote('mftBcEPBkrKZ8Gys5yyDfq8jXCydjf5MtZ', 'luyao'))
     #print(rpc_connection.startforging('mftBcEPBkrKZ8Gys5yyDfq8jXCydjf5MtZ'))
     #print(rpc_connection.stopforging())
-    #print(rpc_connection.sendtoaddress('mnDemujchwMCgDu7xCjDK7ZbsJqC32L1CG', 0.0001))
+    #print(rpc_connection.sendtoaddress('mtUjv2TWT2wMQ86LoshoK5U8dPTL213Zep', 0.00001))
     #print(rpc_connection.dumpprivkey('mftBcEPBkrKZ8Gys5yyDfq8jXCydjf5MtZ'))
 
-    #tx = rpc_connection.gettransactionnew('c30d4992d456efcc4bc390d3facaf68138fe0729d84d96cf1560d1e7479c2c57')
     # print(rpc_connection.help())
     # best_block_hash = rpc_connection.getbestblockhash()
     # blockhash = rpc_connection.getblockhash(2050113)
--- src/test/test_me.cpp
+++ src/test/test_me.cpp
@@ -15,7 +15,103 @@
 #include "keystore.h"
 #include "validation.h"
 
+#include <univalue.h>
+#include "rpc/server.h"
+#include "rpc/client.h"
+
+#include <boost/assign/list_of.hpp>
+#include <boost/algorithm/string.hpp>
+
+// https://www.qiujiawei.com/crypto-1-ecc/
+// https://www.cnblogs.com/X-knight/p/9153209.html
+
 BOOST_FIXTURE_TEST_SUITE(me_tests, BasicTestingSetup)
+
+using namespace std;
+
+UniValue test_createrawtransaction() {
+    std::string args = "createrawtransaction [{\"txid\":\"db949bca827cbe1f5f18848cf55cbc2eedd3cf9633fb9b88e546171087cef4ff\",\"vout\":0}] {\"mtUjv2TWT2wMQ86LoshoK5U8dPTL213Zep\":0.0001}";
+    std::vector<std::string> vArgs;
+    boost::split(vArgs, args, boost::is_any_of(" \t"));
+    std::string strMethod = vArgs[0];
+    vArgs.erase(vArgs.begin());
+    JSONRPCRequest request;
+    request.params = RPCConvertValues(strMethod, vArgs);
+    
+    RPCTypeCheck(request.params, boost::assign::list_of(UniValue::VARR)(UniValue::VOBJ)(UniValue::VNUM), true);
+    if (request.params[0].isNull() || request.params[1].isNull())
+        throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid parameter, arguments 1 and 2 must be non-null");
+
+    UniValue inputs = request.params[0].get_array();
+    UniValue sendTo = request.params[1].get_obj();
+
+    CMutableTransaction rawTx;
+
+    if (request.params.size() > 2 && !request.params[2].isNull()) {
+        int64_t nLockTime = request.params[2].get_int64();
+        if (nLockTime < 0 || nLockTime > std::numeric_limits<uint32_t>::max())
+            throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid parameter, locktime out of range");
+        rawTx.nLockTime = nLockTime;
+    }
+
+    for (unsigned int idx = 0; idx < inputs.size(); idx++) {
+        const UniValue& input = inputs[idx];
+        const UniValue& o = input.get_obj();
+
+        uint256 txid = ParseHashO(o, "txid");
+
+        const UniValue& vout_v = find_value(o, "vout");
+        if (!vout_v.isNum())
+            throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid parameter, missing vout key");
+        int nOutput = vout_v.get_int();
+        if (nOutput < 0)
+            throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid parameter, vout must be positive");
+
+        uint32_t nSequence = (rawTx.nLockTime ? std::numeric_limits<uint32_t>::max() - 1 : std::numeric_limits<uint32_t>::max());
+
+        // set the sequence number if passed in the parameters object
+        const UniValue& sequenceObj = find_value(o, "sequence");
+        if (sequenceObj.isNum()) {
+            int64_t seqNr64 = sequenceObj.get_int64();
+            if (seqNr64 < 0 || seqNr64 > std::numeric_limits<uint32_t>::max())
+                throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid parameter, sequence number is out of range");
+            else
+                nSequence = (uint32_t)seqNr64;
+        }
+
+        CTxIn in(COutPoint(txid, nOutput), CScript(), nSequence);
+
+        rawTx.vin.push_back(in);
+    }
+
+    set<CBitcoinAddress> setAddress;
+    vector<string> addrList = sendTo.getKeys();
+    BOOST_FOREACH(const string& name_, addrList) {
+
+        if (name_ == "data") {
+            std::vector<unsigned char> data = ParseHexV(sendTo[name_].getValStr(),"Data");
+
+            CTxOut out(0, CScript() << OP_RETURN << data);
+            rawTx.vout.push_back(out);
+        } else {
+            CBitcoinAddress address(name_);
+            if (!address.IsValid())
+                throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, string("Invalid Bitcoin address: ")+name_);
+
+            if (setAddress.count(address))
+                throw JSONRPCError(RPC_INVALID_PARAMETER, string("Invalid parameter, duplicated address: ")+name_);
+            setAddress.insert(address);
+
+            CScript scriptPubKey = GetScriptForDestination(address.Get());
+            CAmount nAmount = AmountFromValue(sendTo[name_]);
+
+            CTxOut out(nAmount, scriptPubKey);
+            rawTx.vout.push_back(out);
+        }
+    }
+
+    return EncodeHexTx(rawTx);
+}
 
 BOOST_AUTO_TEST_CASE(me_1)
 {
@@ -41,6 +137,9 @@ BOOST_AUTO_TEST_CASE(me_1)
     std::vector<unsigned char> vchSig;
     key.SignCompact(ss.GetHash(), vchSig);
     std::cout << EncodeBase64(&vchSig[0], vchSig.size()) << std::endl;
+
+    UniValue rawtransaction = test_createrawtransaction();
+    std::cout << rawtransaction.get_str() << std::endl;
 }
 
 BOOST_AUTO_TEST_SUITE_END()
